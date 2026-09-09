c944cb186c9d9ad71eeee0fde327afa341df9de2
franksky1207 <131867380+franksky1207@users.noreply.github.com>
2026-09-09 22:57:00 +0800

    Delete obsolete world title override

--- worldexpansion.js
+++
@@ -1,8 +0,0 @@
-(function(){
- const baseHomePageForWorld=homePage;
- homePage=function(){return baseHomePageForWorld().replace("純文字 RPG","文明戰線");};
- document.title="文明戰線";
- const brand=document.getElementById("brandTitle");
- if(brand)brand.textContent="文明戰線";
- setTimeout(()=>{if(typeof render==="function")render();},0);
-})();
