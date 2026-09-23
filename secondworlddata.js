4eecadb89f12c38e76326c35332c66c3d299fbec
franksky1207 <131867380+franksky1207@users.noreply.github.com>
2026-09-23 12:08:50 +0800

    Restore second world data after batch 4 edit guard

--- secondworlddata.js
+++ secondworlddata.js
@@ -1,1 +1,7 @@
-PLACEHOLDER
+(function(){
+ const VERSION=1;
+ const BOSS_COUNT=100;
+ const REGION_COUNT=10;
+ const EQUIPMENT_SLOTS=Object.freeze(["weapon","helmet","armor","shoes","accessory"]);
+ const REGIONS=Object.freeze([]);
+})();
